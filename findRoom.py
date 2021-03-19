bc4f20df146e68c1a2b1d9b4ea45ad5dc5506669
codewithbLanksS <apiwat.pu@ku.th>
2021-03-20 03:15:33 +0700

    project

---
+++ findRoom.py
@@ -0,0 +1,34 @@
+import json
+from firebase import firebase
+
+print("1.Private room")
+print("2.Package room")
+print("3.Group room")
+x = input("Input you TYPE of room you want to see:  ")
+
+firebase = firebase.FirebaseApplication(
+    "https://sleepyhotelzoo-default-rtdb.firebaseio.com/", None
+)
+
+
+if x == "1":
+    print("########### Private room ###########")
+    room_id = input("Input you ID room you want to see:  ")
+    result = firebase.get("/Hotel_room/PrivateROOM", room_id)
+    print(result)
+elif x == "2":
+    print("########### Package room ###########")
+    room_id = input("Input you ID room you want to see:  ")
+    result = firebase.get("/Hotel_room/PackageROOM", room_id)
+    print(result)
+elif x == "3":
+    print("########### Group room ###########")
+    room_id = input("Input you ID room you want to see:  ")
+    result = firebase.get("/Hotel_room/GroupROOM", room_id)
+    print(result)
+else:
+    print("ERROR")
+
+
+result = firebase.get("/Hotel_room", room_id)
+print(result)
